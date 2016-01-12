55779ef680e814390ad63af3fad7185dc536faea
silan-liu <413769707@qq.com>
2016-01-12 17:27:50 +0800

    first commit

---
+++ RGBColor/RGBColor.h
@@ -0,0 +1,19 @@
+//
+//  RGBColor.h
+//  RGBColor
+//
+//  Created by liusilan on 16/1/12.
+//  Copyright © 2016年 YY Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RGBColor.
+FOUNDATION_EXPORT double RGBColorVersionNumber;
+
+//! Project version string for RGBColor.
+FOUNDATION_EXPORT const unsigned char RGBColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RGBColor/PublicHeader.h>
+
+
---
+++ RGBColor/RGBColor.swift
@@ -0,0 +1,9 @@
+//
+//  RGBColor.swift
+//  RGBColor
+//
+//  Created by liusilan on 16/1/12.
+//  Copyright © 2016年 YY Inc. All rights reserved.
+//
+
+import Foundation
